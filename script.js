6876b3ee2f9b5a484ee46b408140bae0dd8b29ea
Goktug Gude <gudegoktug@hotmail.com>
2025-11-18 18:18:43 +0300

    Created

---
+++ script.js
@@ -0,0 +1,28 @@
+const hamburger = document.getElementById("hamburger");
+const mobilmenu = document.getElementById("mobileMenu");
+const hamburgerClose = document.getElementById("hamburger-close");
+const ball = document.getElementById("ball")
+const items = document.querySelectorAll("body,.navbar-wrapper,.navbar-wrapper a,.logo-wrapper,.logo-wrapper img,.slogan-text p,.toggle-ball,.all-products,p,.card-picture img");
+
+hamburger.addEventListener("click", (e) => {
+    e.stopPropagation(); // tıklamayı sayfaya iletme
+    mobileMenu.classList.toggle("active");
+});
+
+mobileMenu.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+hamburgerClose.addEventListener("click",(e)=>{
+    mobileMenu.classList.toggle("active");
+    
+});
+    
+
+document.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+});
+
+ball.addEventListener("click", function(){
+items.forEach((item)=> item.classList.toggle("dark"));
+})
