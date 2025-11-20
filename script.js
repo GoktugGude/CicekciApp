bba990479754cbc5858df4df4665793d016bc8c5
Goktug Gude <gudegoktug@hotmail.com>
2025-11-20 18:15:58 +0300

    responsiveupdate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById("hamburger");
 const mobilmenu = document.getElementById("mobileMenu");
 const hamburgerClose = document.getElementById("hamburger-close");
 const ball = document.getElementById("ball")
-const items = document.querySelectorAll("body,.navbar-wrapper,.navbar-wrapper a,.logo-wrapper,.logo-wrapper img,.slogan-text p,.toggle-ball,.all-products,p,.card-picture img");
+const items = document.querySelectorAll("body,.navbar-wrapper,.navbar-wrapper a,.logo-wrapper,.logo-wrapper img,.slogan-text p,.toggle-ball,.all-products,p,.card-picture img,footer,.footer-container");
 
 hamburger.addEventListener("click", (e) => {
     e.stopPropagation(); // tıklamayı sayfaya iletme
